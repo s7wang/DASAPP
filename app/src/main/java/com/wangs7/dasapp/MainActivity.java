8fb964ed030636754ec351cd93b728610efc2d07
wangs7__ <1074790930@qq.com>
2022-12-13 18:45:58 +0800

    Create project

---
+++ app/src/main/java/com/wangs7/dasapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wangs7.dasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
