0d0c01117f53313091373db757f5ba02617fc064
wangs7__ <1074790930@qq.com>
2023-01-05 19:12:17 +0800

    添加Camera2相关

--- app/src/main/java/com/wangs7/dasapp/SendActivity.java
+++ app/src/main/java/com/wangs7/dasapp/SendActivity.java
@@ -3,8 +3,17 @@ package com.wangs7.dasapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.wangs7.dasapp.camera.Camera2Tools;
+import com.wangs7.projectv1.camera2.AutoFitSurfaceView;
 
 public class SendActivity extends AppCompatActivity {
+
+
+    private Camera2Tools mCamera2Tools;
+    private Button startBt;
+    private AutoFitSurfaceView autoFitSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
